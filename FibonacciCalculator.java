a03af3d287afc649bc481d739d4fcb781a9ea097
Olin <leonamusictosufferby@gmail.com>
2019-01-25 19:03:56 -0500

    Initial sharing of project

---
+++ FibonacciCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * J
+ */
+public class FibonacciCalculator {
+
+    public static void main(String args[]) {
+    
+        System.out.println("F"); 
+    }
+  
+}
